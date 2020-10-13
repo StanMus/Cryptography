dc824dbec76b22660dfc309090cea4ae616362cc
StanMus <59773085+StanMus@users.noreply.github.com>
2020-10-13 12:57:39 +0300

    Update MyM.py
    Добавлена функция Function(x)

--- MyM.py
+++ MyM.py
@@ -3,8 +3,8 @@ from tkinter import *
 from tkinter.filedialog import *
 import random
 
-def fu(a):
-    return a*2
+def Function(x):
+    return x^2-1
 
 def get_coding(letter, max_rnd, lst):
     rnd_index = random.randint(0, max_rnd)
