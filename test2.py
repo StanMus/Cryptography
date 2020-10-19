0d50d71f716c2853e8e6b958b5d28dc90523fa09
StanMus <59773085+StanMus@users.noreply.github.com>
2020-10-19 10:25:45 +0300

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,11 @@
+def get_lst():
+    txt = 'aydfdfmvjdsajafads'
+    cod_txt = []
+    for i in txt:
+        cod_txt.append(str(i))
+    line = 'hdfnnejemdla;sjdrm.asf'
+    lst = []
+    for i in line:
+        lst.append(str(i))
+
+get_lst()
